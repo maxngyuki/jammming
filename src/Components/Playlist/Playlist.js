b7caebd530e747c69bb0b4740a940885edffe8b9
Max <maxngyuki@gmail.com>
2023-01-15 17:08:31 +0100

    Finish up to Step 96

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Playlist.css';
-import {TrackList} from '../..TrackList/TrackList';
+import {TrackList} from '../TrackList/TrackList';
 
 export class Playlist extends React.Component{
     constructor(props){
