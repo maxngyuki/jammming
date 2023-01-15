89a92040545904815f32da118a1ad2d7df8ae7af
Max <maxngyuki@gmail.com>
2023-01-15 15:56:58 +0100

    Finished up to Step 35

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -14,11 +14,11 @@ export class Track extends React.Component{
     render(){
         return(
             <div className = "Track">
-            <div className ="Track-information">
-                <h3><!-- track name will go here --></h3>
-                <p><!-- track artist will go here--> | <!-- track album will go here --></p>
-            </div>
-            <button className ="Track-action"><!-- + or - will go here --></button>
+                <div className ="Track-information">
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.artist}</p>
+                </div>
+                {this.renderAction()}
             </div>
         );
 
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,12 @@ export class TrackList extends React.Component{
     render(){
         return(
             <div className="TrackList">
-                {/*<-- You will add a map method that renders a set of Track components  -->*/}
+                {this.props.tracks ? this.props.tracks.map(track => {
+                    return <Track track={track} onRemove={this.props.onRemove} 
+                    onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} 
+                     key={track.id} />;
+                }) : null
+                }
             </div>
         );
     }
